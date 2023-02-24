7abdff1f6bd691135d7bd5b4faa6b08cd54ca2ce
keith <25635@ma-web.nl>
2023-02-24 12:11:42 +0100

    flex-game

---
+++ flex-game/flex-game/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace flex_game
+{
+    internal class Program
+    {
+        //class variable
+        string programTitle = "variable demo";
+        int programWindowsPoxx = 100;
+        static void Main(string[] args)
+        {
+            //type name
+
+
+            //local variables
+            int xPos = 100;
+
+            xPos = xPos + 10;
+
+            Console.WriteLine(xPos);
+        }
+    }
+}
